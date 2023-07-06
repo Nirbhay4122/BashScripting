96695ec833ea9fb81314d68d7e9baa4bf5d7fdf1
Nirbhay Singh <69494994+Nirbhay4122@users.noreply.github.com>
2023-07-06 23:15:18 +0530

    mongo condition

--- install_mongo.sh
+++ install_mongo.sh
@@ -14,43 +14,63 @@ START_MONGO_SERVICE() {
     sudo systemctl enable mongod.service
 }
 INSTALL_MONGO() {
-    DETECT_OS; sudo apt-get install gnupg curl -y; echo
-    read -p "Select MongoDB Version - [ 4.4, 5.0, 6.0 ] " MONGO_VERSION;
-    if [ "${OS_ID,,}" == "ubuntu" ]; then
-        if [ "${OS_VERSION%.*}" == "22" ]; then
-            echo "Detected ${OS_ID,,}-${OS_VERSION}, there will be only availabe mongoDB version { 6.0 }"
-            read -p "To continue press [Y/y] or press any key to exit: " CONDITION
-            if [ "${CONDITION}" == "y" ] || [ "${CONDITION}" == "Y" ]; then
-                MONGO_VERSION=6.0
+    DETECT_OS; if [ "${OS_ID,,}" == "ubuntu" ] || [ "${OS_ID,,}" == "debian" ]; then
+        sudo apt-get install gnupg curl -y; echo
+        read -p "Select MongoDB Version - [ 4.4, 5.0, 6.0 ] " MONGO_VERSION;
+        if [ "${OS_ID,,}" == "ubuntu" ]; then
+            if [ "${OS_VERSION%.*}" == "22" ]; then
+                echo "Detected ${OS_ID,,}-${OS_VERSION}, there will be only availabe mongoDB version { 6.0 }"
+                read -p "To continue press [Y/y] or press any key to exit: " CONDITION
+                if [ "${CONDITION}" == "y" ] || [ "${CONDITION}" == "Y" ]; then
+                    MONGO_VERSION=6.0
+                else
+                    echo "[Info] Exiting setup! - Aborted by ${USER}"; exit 1;
+                fi
+            fi
+            curl -fsSL https://pgp.mongodb.com/server-${MONGO_VERSION}.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-${MONGO_VERSION}.gpg --dearmor
+            echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-${MONGO_VERSION}.gpg ] https://repo.mongodb.org/apt/ubuntu ${OS_CODENAME}/mongodb-org/${MONGO_VERSION} multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-${MONGO_VERSION}.list
+        elif [ "${OS_ID,,}" == "debian" ]; then
+            if [ "${MONGO_VERSION}" == "4.4" ] && [ "${OS_CODENAME}" == "bullseye" ]; then
+                echo "[Exiting] MongoDB v${MONGO_VERSION} is Not available for ${OS_ID,,}-${OS_VERSION}."
+                echo "[Info] v5.0, v6.0 is available for ${OS_ID,,}-${OS_VERSION}."
+                echo "[Info] Please choose right version to complete the mongoDB installation."; exit 1;
+            elif [ "${MONGO_VERSION}" == "6.0" ] && [ "${OS_CODENAME}" == "stretch" ]; then
+                echo "[Info] MongoDB v${MONGO_VERSION} is Not available for ${OS_ID,,}-${OS_VERSION}."
+                echo "[Info] v4.4, v5.0 is available for ${OS_ID,,}-${OS_VERSION}."
+                echo "[Info] Please choose right version to complete the mongoDB installation."; exit 1;
+            elif [ "${MONGO_VERSION}" == "5.0" ] && [ "${OS_CODENAME}" == "stretch" ]; then
+                curl -fsSL https://pgp.mongodb.com/server-${MONGO_VERSION}.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-${MONGO_VERSION}.gpg --dearmor
+                echo "deb http://repo.mongodb.org/apt/debian ${OS_CODENAME}/mongodb-org/${MONGO_VERSION} main" | sudo tee /etc/apt/sources.list.d/mongodb-org-${MONGO_VERSION}.list
             else
-                echo "[Info] Exiting setup!"; exit 1;
+                curl -fsSL https://pgp.mongodb.com/server-${MONGO_VERSION}.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-${MONGO_VERSION}.gpg --dearmor
+                echo "deb [ signed-by=/usr/share/keyrings/mongodb-server-${MONGO_VERSION}.gpg ] http://repo.mongodb.org/apt/debian ${OS_CODENAME}/mongodb-org/${MONGO_VERSION} main" | sudo tee /etc/apt/sources.list.d/mongodb-org-${MONGO_VERSION}.list #10
             fi
         fi
-        curl -fsSL https://pgp.mongodb.com/server-${MONGO_VERSION}.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-${MONGO_VERSION}.gpg --dearmor
-        echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-${MONGO_VERSION}.gpg ] https://repo.mongodb.org/apt/ubuntu ${OS_CODENAME}/mongodb-org/${MONGO_VERSION} multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-${MONGO_VERSION}.list
-    elif [ "${OS_ID,,}" == "debian" ]; then
-        if [ "${MONGO_VERSION}" == "4.4" ] && [ "${OS_CODENAME}" == "bullseye" ]; then
-            echo "[Exiting] MongoDB v${MONGO_VERSION} is Not available for ${OS_ID,,}-${OS_VERSION}."
-            echo "[Info] v5.0, v6.0 is available for ${OS_ID,,}-${OS_VERSION}."
-            echo "[Info] Please choose right version to complete the mongoDB installation."; exit 1;
-        elif [ "${MONGO_VERSION}" == "6.0" ] && [ "${OS_CODENAME}" == "stretch" ]; then
-            echo "[Info] MongoDB v${MONGO_VERSION} is Not available for ${OS_ID,,}-${OS_VERSION}."
-            echo "[Info] v4.4, v5.0 is available for ${OS_ID,,}-${OS_VERSION}."
-            echo "[Info] Please choose right version to complete the mongoDB installation."; exit 1;
-        elif [ "${MONGO_VERSION}" == "5.0" ] && [ "${OS_CODENAME}" == "stretch" ]; then
-            curl -fsSL https://pgp.mongodb.com/server-${MONGO_VERSION}.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-${MONGO_VERSION}.gpg --dearmor
-            echo "deb http://repo.mongodb.org/apt/debian ${OS_CODENAME}/mongodb-org/${MONGO_VERSION} main" | sudo tee /etc/apt/sources.list.d/mongodb-org-${MONGO_VERSION}.list
-        else
-            curl -fsSL https://pgp.mongodb.com/server-${MONGO_VERSION}.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-${MONGO_VERSION}.gpg --dearmor
-            echo "deb [ signed-by=/usr/share/keyrings/mongodb-server-${MONGO_VERSION}.gpg ] http://repo.mongodb.org/apt/debian ${OS_CODENAME}/mongodb-org/${MONGO_VERSION} main" | sudo tee /etc/apt/sources.list.d/mongodb-org-${MONGO_VERSION}.list #10
-        fi
+        sudo apt-get update
+        sudo apt-get install -y mongodb-org
+        sleep 5; START_MONGO_SERVICE;
+    else
+        echo "[Info] System detected other than Ubuntu/Debian exiting..."; exit 1;
     fi
-    sudo apt-get update
-    sudo apt-get install -y mongodb-org
-    sleep 5; START_MONGO_SERVICE;
 }
-if dpkg -s mongodb-org >/dev/null 2>&1; then
-    echo  "[Info] MongoDB Already Present, to install newer version remove older version first."; exit 1;
+if dpkg -s mongodb-org >/dev/null 2>&1; then echo "";
+    echo "====================================================================================="
+    echo  "[Info] MongoDB Already Present, to install newer version remove older version first."
+    echo "* -----------------------------------------------------------------------------------"
+    echo "* To check mongoDB service is running or not - run 'systemctl status mongod.service'"
+    echo "* To debug and see the logs - cd /var/log/mongodb"
+    echo "* To stop or restart the mongo service - 'systemctl stop mongod.service'"
+    echo "* - 'systemctl restart mongod.service'"
+    echo "====================================================================================="
 else
-    INSTALL_MONGO;
+    INSTALL_MONGO; if [ "$?" == "0" ]; then echo "";
+    echo "====================================================================================="
+    echo "* mongoDB - ${MONGO_VERSION} installed successfully."
+    echo "* -----------------------------------------------------------------------------------"
+    echo "* To check mongoDB service is running or not - run 'systemctl status mongod.service'"
+    echo "* To debug and see the logs - cd /var/log/mongodb"
+    echo "* To stop or restart the mongo service - 'systemctl stop mongod.service'"
+    echo "* - 'systemctl restart mongod.service'"
+    echo "====================================================================================="
+    fi
 fi
--- pract.sh
+++ pract.sh
@@ -6,3 +6,20 @@ pkgs=('curl' 'wget' 'python3' 'sed' 'grep')
 # RUN_COMMAND "echo Installing basic dependencies."
 for pkg in ${pkgs[@]}; do RUN_COMMAND "PKG_INSTALL ${pkg}"; done
 
+
+
+# To get extracted dir
+#!/bin/bash
+
+# Extract the tar file
+tar -xf file1.tar
+
+# Find the directory name
+dir_name=$(tar -tf file1.tar | head -n 1 | sed -e 's@/.*@@')
+
+# Run 'cd' command to the new directory
+cd "$dir_name"
+
+# Do whatever you need to do in the new directory
+
+# Rest of your script...
